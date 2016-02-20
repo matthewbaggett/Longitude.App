c50e739fbc0118133e433a0ab719b155204703f1
Matthew Baggett <matthew@baggett.me>
2016-02-20 03:48:23 +0000

    Initial commit

---
+++ app/src/main/java/io/thru/longitude/Friend.java
@@ -0,0 +1,8 @@
+package io.thru.longitude;
+
+
+public class Friend {
+    private String firstName;
+    private String lastName;
+    private Location location;
+}
---
+++ app/src/main/java/io/thru/longitude/Location.java
@@ -0,0 +1,12 @@
+package io.thru.longitude;
+
+
+public class Location {
+    private float x;
+    private float y;
+
+    public void setLocation(float x, float y){
+        this.x = x;
+        this.y = y;
+    }
+}
